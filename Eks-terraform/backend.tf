82710013d13836d0f2e7e35e7fa47499a8f1f4bd
Madhav Kacha <117278549+madhavkacha@users.noreply.github.com>
2024-05-26 19:50:25 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mys3bucketforterraform"    # Replace with your actual S3 bucket name
+    bucket = "mytestbucket5684"    # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
